192ba1c1c05d421cd45fb30fc2bca5532fb1421f
bobby633 <bobbymitchell222@gmail.com>
2023-09-22 12:19:35 +0100

    first commit

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+
+from .views import register_view,login_view
+
+
+
+
+app_name='accounts'
+urlpatterns = [
+    path("",register_view,name = 'register'),
+    path("login/",login_view,name = 'login'),
+]
---
+++ accounts/views.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import  login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+def register_view(request):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        return redirect('/login')
+    context = {"form": form}
+    return render(request, "accounts/register.html", context)
+
+
+def login_view(request):
+    return render (request,'accounts/login.html')
