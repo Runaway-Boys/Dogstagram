ff0281c472058e57cbd77df57a2affffcb95bac1
bobby633 <bobbymitchell222@gmail.com>
2023-09-22 13:08:35 +0100

    added dog breed csv file

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class DogAccount(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    def __str__(user):
+        return user
