20910f2cac5fb467ca6045c924fdfb27e95fe6f3
bobby633 <bobbymitchell222@gmail.com>
2023-09-24 10:39:29 +0100

    removed node module

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,20 @@
 
 from django.urls import path
 
-from .views import register_view,login_view
+from .views import (
+    register_view,
+    login_view,
+    index,
+    
+    
+    )
 
 
 
 
 app_name='accounts'
 urlpatterns = [
-    path("",register_view,name = 'register'),
+    path("",index,name = 'index'),
+    path("register/",register_view,name = 'register'),
     path("login/",login_view,name = 'login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import  login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
-
+from django.http import HttpResponse
 # Create your views here.
 
 def register_view(request):
@@ -14,3 +14,7 @@ def register_view(request):
 
 def login_view(request):
     return render (request,'accounts/login.html')
+
+
+def index(request):
+    return render (request,'index.html')
