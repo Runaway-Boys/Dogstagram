92ef34860c62e93b8c0bd0daccfb2093a1186f0c
bobby633 <bobbymitchell222@gmail.com>
2023-09-30 11:57:59 +0100

    updated model account

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 User = get_user_model()
 class DogAccount(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_user = models.IntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
     breed = models.CharField(max_length=50,blank=True,null=True)
     bio = models.TextField(blank=True)
     profileimg = models.ImageField(upload_to='profile_images', default='default-dog-profile.jpg')
