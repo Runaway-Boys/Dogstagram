2ac4bf0fc2a2e02a2aae552f950c78bdc095232b
bobby633 <bobbymitchell222@gmail.com>
2024-03-05 11:42:34 +0000

    small update

--- dogstagram/settings.py
+++ dogstagram/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
 ]
+#AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
