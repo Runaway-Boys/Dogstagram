64ab07963b53ac3aacd3100d0856a88d085d763b
bobby633 <bobbymitchell222@gmail.com>
2024-03-06 16:54:38 +0000

    using createview

--- accounts/views.py
+++ accounts/views.py
@@ -2,16 +2,26 @@ from django.contrib.auth import  login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.generic import CreateView
+from accounts.models import DogAccount
+from accounts.forms import SignupForm
 import pandas as pd
 import csv
 # Create your views here.
+#creating a signup view page
+class SignupView(CreateView):
+    model = DogAccount
+    form_class = SignupForm
+    success_url = "accounts/login.html"
+    template_name = "accounts/register.html"
 
-def register_view(request):
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-        return redirect('/login')
-    context = {"form": form}
-    return render(request, "accounts/register.html", context)
+#removing reg view
+# def register_view(request):
+#     form = UserCreationForm(request.POST)
+#     if form.is_valid():
+#         return redirect('/login')
+#     context = {"form": form}
+#     return render(request, "accounts/register.html", context)
 
 
 def login_view(request):
