1bab9c9f395f520e1fc7edf4eb13acefe66b9f77
bobby633 <bobbymitchell222@gmail.com>
2024-03-18 18:09:41 +0000

    remove friend view

--- friend/views.py
+++ friend/views.py
@@ -5,7 +5,7 @@ import json
 # Create your views here.
 
 from accounts.models import DogAccount
-from friend.models import FriendRequest
+from friend.models import FriendRequest,FriendList
 
 
 def friend_requests(request, *args, **kwargs):
@@ -86,3 +86,46 @@ def accept_friend_request(request, *args, **kwargs):
 		# should never happen
 		payload['response'] = "You must be authenticated to accept a friend request."
 	return HttpResponse(json.dumps(payload), content_type="application/json")
+
+def remove_friend(request, *args, **kwargs):
+	user = request.user
+	payload = {}
+	if request.method == "POST" and user.is_authenticated:
+		user_id = request.POST.get("receiver_user_id")
+		if user_id:
+			try:
+				removee = DogAccount.objects.get(pk=user_id)
+				friend_list = FriendList.objects.get(user=user)
+				friend_list.unfriend(removee)
+				payload['response'] = "Successfully removed that friend."
+			except Exception as e:
+				payload['response'] = f"Something went wrong: {str(e)}"
+		else:
+			payload['response'] = "There was an error. Unable to remove that friend."
+	else:
+		# should never happen
+		payload['response'] = "You must be authenticated to remove a friend."
+	return HttpResponse(json.dumps(payload), content_type="application/json")
+# def decline_friend_request(request, *args, **kwargs):
+# 	user = request.user
+# 	payload = {}
+# 	if request.method == "GET" and user.is_authenticated:
+# 		friend_request_id = kwargs.get("friend_request_id")
+# 		if friend_request_id:
+# 			friend_request = FriendRequest.objects.get(pk=friend_request_id)
+# 			# confirm that is the correct request
+# 			if friend_request.receiver == user:
+# 				if friend_request: 
+# 					# found the request. Now decline it
+# 					updated_notification = friend_request.decline()
+# 					payload['response'] = "Friend request declined."
+# 				else:
+# 					payload['response'] = "Something went wrong."
+# 			else:
+# 				payload['response'] = "That is not your friend request to decline."
+# 		else:
+# 			payload['response'] = "Unable to decline that friend request."
+# 	else:
+# 		# should never happen
+# 		payload['response'] = "You must be authenticated to decline a friend request."
+# 	return HttpResponse(json.dumps(payload), content_type="application/json")
